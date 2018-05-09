b52d7506b9c89b2bc5d60ab68aab9c6dfad12cf1
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-05-09 10:44:51 -0600

    Tests as specified in Iterations 0 through 3

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -1,1 +1,35 @@
 # YOUR TESTS GO IN HERE!
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/translator.rb'
+require './lib/translate.rb'
+require 'pry'
+
+class TranslatorTest < Minitest::Test
+
+  def test_it_exists
+    translator = Translate.new
+    assert_instance_of Translate, translator
+  end
+
+  def test_it_translates
+    translator = Translate.new
+    assert_equal "......-...-..--- .-----.-..-..-..", translator.eng_to_morse("hello world")
+  end
+
+  def test_it_handles_caps_and_numbers
+    translator = Translate.new
+    assert_equal "......-...-..--- .-----.-..-..-..", translator.eng_to_morse("Hello World")
+    assert_equal "-......-.. .-.-.. ...-- ..........--....", translator.eng_to_morse("There are 3 ships")
+  end
+
+  def test_it_can_take_input_from_files
+    translator = Translate.new
+    assert_equal ".. .--- ..-. .- ..-....-...", translator.from_file("input.txt")
+  end
+
+  def test_it_can_translate_back
+    translator = Translate.new
+    assert_equal "hello world", translator.morse_to_eng(".... . .-.. .-.. ---  .-- --- .-. .-.. -..")
+  end
+end
